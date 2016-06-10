b83239e11953bc2db705f60a28631fe5d9829f0b
hyeyoung <rgd1359@naver.com>
2016-06-10 12:49:03 +0900

    Added file resource to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,15 @@
 node 'hyeyoung'{
-  include sys_info
+#  include sys_info
 }
 
+nod 'vm01.linuxmaster.com' {
+  file {'/tmp/testserver/:
+    content => "This is a test server.\n",
+  }
+}
+
+nod /^vm[0-9]+\.linuxmaster\.com$/ {
+}
 file {'/tmp/HelloPuppetMaster':
 	content => "Hello, puppet master!\n",
 }
