c8ada42f0a9777d0894a71c35c0d2c5c6ccfbdee
hyeyoung <rgd1359@naver.com>
2016-06-10 12:50:25 +0900

    Added sshd module to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,10 +6,12 @@ nod 'vm01.linuxmaster.com' {
   file {'/tmp/testserver/:
     content => "This is a test server.\n",
   }
+  include sshd
 }
 
 nod /^vm[0-9]+\.linuxmaster\.com$/ {
 }
+
 file {'/tmp/HelloPuppetMaster':
 	content => "Hello, puppet master!\n",
 }
